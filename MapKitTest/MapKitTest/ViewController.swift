75eb13b4792c2ed9126e97dbf42005fdbae4294b
dami <dami8789@gmail.com>
2023-07-23 20:34:26 +0900

    애플제공 장소 너무 적음 다른 데이터소스 사용해야함

--- MapKitTest/MapKitTest/ViewController.swift
+++ MapKitTest/MapKitTest/ViewController.swift
@@ -11,71 +11,120 @@ import CoreLocation
 import SnapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
-
+    
     private let mapView = MKMapView()
-        private let locationManager = CLLocationManager()
-        private let myLocationButton = UIButton()
-        private var bottomSheetView: BottomSheetView?
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-
-            // 지도 뷰 설정
-            mapView.showsUserLocation = true
-            mapView.delegate = self
-
-            // 위치 관리자 설정
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
-
-            // 위치 권한 요청
-            locationManager.requestWhenInUseAuthorization()
-
-            // SnapKit을 사용하여 레이아웃 설정
-            view.addSubview(mapView)
-            mapView.snp.makeConstraints { make in
-                make.edges.equalToSuperview()
-            }
-
-            // 내 위치로 이동하는 버튼 설정
-            myLocationButton.setTitle("내 위치", for: .normal)
-            myLocationButton.setTitleColor(.white, for: .normal)
-            myLocationButton.backgroundColor = .blue
-            myLocationButton.layer.cornerRadius = 8.0
-            myLocationButton.addTarget(self, action: #selector(showMyLocation), for: .touchUpInside)
-
-            view.addSubview(myLocationButton)
-            myLocationButton.snp.makeConstraints { make in
-                make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-16)
-                make.centerX.equalToSuperview()
-                make.width.equalTo(120)
-                make.height.equalTo(40)
-            }
+    private let locationManager = CLLocationManager()
+    private let myLocationButton = UIButton()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 지도 뷰 설정
+        mapView.showsUserLocation = true
+        mapView.delegate = self
+        
+        // 위치 관리자 설정
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // 위치 권한 요청
+        locationManager.requestWhenInUseAuthorization()
+        
+        // SnapKit을 사용하여 레이아웃 설정
+        view.addSubview(mapView)
+        mapView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
         }
+        
+        // 내 위치로 이동하는 버튼 설정
+        myLocationButton.setTitle("내 위치", for: .normal)
+        myLocationButton.setTitleColor(.white, for: .normal)
+        myLocationButton.backgroundColor = .blue
+        myLocationButton.layer.cornerRadius = 8.0
+        myLocationButton.addTarget(self, action: #selector(showMyLocation), for: .touchUpInside)
+        
+        view.addSubview(myLocationButton)
+        myLocationButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-16)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(120)
+            make.height.equalTo(40)
+        }
+    }
+    
+    // MARK: - 내 위치로 이동하는 액션
+    
+    @objc private func showMyLocation() {
+        if let userLocation = mapView.userLocation.location?.coordinate {
+                let region = MKCoordinateRegion(center: userLocation, latitudinalMeters: 500, longitudinalMeters: 500)
 
-        // MARK: - 내 위치로 이동하는 액션
-
-        @objc private func showMyLocation() {
-            if let userLocation = mapView.userLocation.location?.coordinate {
-                let region = MKCoordinateRegion(center: userLocation, latitudinalMeters: 1000, longitudinalMeters: 1000)
                 mapView.setRegion(region, animated: true)
+            mapView.removeOverlays(mapView.overlays)
 
-                // 50미터 반경의 동그라미를 그리기 위해 코드 추가
-                let circle = MKCircle(center: userLocation, radius: 50)
+                // 100미터 반경의 동그라미를 그리기 위해 코드 추가
+                let circle = MKCircle(center: userLocation, radius: 100)
                 mapView.addOverlay(circle)
-
-                // 바텀 시트를 생성하고 지도 아래에 추가
-                bottomSheetView = BottomSheetView()
-                view.addSubview(bottomSheetView!)
-                bottomSheetView?.snp.makeConstraints { make in
-                    make.leading.trailing.equalToSuperview()
-                    make.height.equalTo(200) // 바텀 시트의 높이 설정
-                    make.bottom.equalTo(view.safeAreaLayoutGuide) // 바텀 시트를 지도 아래에 위치
-                }
+                
+                // 원 안에 있는 장소들 검색하여 표시
+                searchPlacesWithinCircle(center: userLocation, radius: 100)
             }
-        }
-    // MARK: - CLLocationManagerDelegate
+    }
+    private func searchPlacesWithinCircle(center: CLLocationCoordinate2D, radius: CLLocationDistance) {
+        // 기존에 표시된 장소들을 지도에서 제거
+        mapView.removeAnnotations(mapView.annotations)
+        
+        // MKLocalSearchRequest 생성
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "cafe" // 검색할 장소 종류
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: radius, longitudinalMeters: radius)
+            request.region = region
+        
+        // MKLocalSearch 객체 생성
+        let search = MKLocalSearch(request: request)
+        
+        // 장소 검색 시작
+        search.start { [weak self] (response, error) in
+            guard let self = self, let response = response else {
+                print("Error searching places: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            
+//            // 검색 결과 중 장소들을 지도에 표시
+//            for item in response.mapItems {
+//                self.addAnnotationToMap(item)
+//              //  print(item)
+//            }
+            // 검색 결과 중 원 안에 있는 카페들을 지도에 표시
+                  for item in response.mapItems {
+                      let itemLocation = CLLocation(latitude: item.placemark.coordinate.latitude, longitude: item.placemark.coordinate.longitude)
+                      let centerLocation = CLLocation(latitude: center.latitude, longitude: center.longitude)
+                      let distance = centerLocation.distance(from: itemLocation)
+                      print(distance)
+                      print(radius)
+                      if distance <= radius {
+                          self.addAnnotationToMap(item)
+                      }
+                  }
 
+            // 검색된 장소들을 바텀 시트에 표시
+            
+            self.showPlacesInBottomSheet(response.mapItems)
+        }
+    }
+
+    // 장소를 지도에 표시하는 메서드
+    private func addAnnotationToMap(_ mapItem: MKMapItem) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = mapItem.placemark.coordinate
+        annotation.title = mapItem.name
+        print(annotation.title)
+        mapView.addAnnotation(annotation)
+    }
+
+    // 검색된 장소들을 바텀 시트에 표시하는 메서드
+    private func showPlacesInBottomSheet(_ places: [MKMapItem]) {
+    }
+    // MARK: - CLLocationManagerDelegate
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         // 위치 업데이트를 받았을 때 호출되는 메서드
         if let location = locations.last {
@@ -84,7 +133,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             mapView.setRegion(region, animated: true)
         }
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         // 위치 권한이 변경되었을 때 호출되는 메서드
         if status == .authorizedWhenInUse {
@@ -92,52 +141,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             locationManager.startUpdatingLocation()
         }
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         // 위치 업데이트 실패 시 호출되는 메서드
         print("Location manager error: \(error.localizedDescription)")
     }
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-         if let circleOverlay = overlay as? MKCircle {
-             let circleRenderer = MKCircleRenderer(circle: circleOverlay)
-             circleRenderer.fillColor = UIColor(red: 0, green: 0, blue: 1, alpha: 0.3) // 동그라미 내부 색상
-             circleRenderer.strokeColor = UIColor.blue // 동그라미 테두리 색상
-             circleRenderer.lineWidth = 1 // 동그라미 테두리 두께
-             return circleRenderer
-         }
-         return MKOverlayRenderer(overlay: overlay)
-     }
-}
-class BottomSheetView: UIView {
-
-    // 여기에 바텀 시트에 들어갈 컨텐츠나 레이아웃을 구성합니다.
-    // 예시로 "바텀 시트 내용"이라는 레이블을 추가하겠습니다.
-    private let contentLabel: UILabel = {
-        let label = UILabel()
-        label.text = "바텀 시트 내용"
-        label.textAlignment = .center
-        label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-        return label
-    }()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-
-        // 바텀 시트의 배경색과 모서리를 설정합니다.
-        backgroundColor = .white
-        layer.cornerRadius = 16
-
-        // 컨텐츠 레이블을 바텀 시트에 추가합니다.
-        addSubview(contentLabel)
-
-        // SnapKit을 사용하여 컨텐츠 레이블을 바텀 시트에 레이아웃합니다.
-        contentLabel.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+        if let circleOverlay = overlay as? MKCircle {
+            let circleRenderer = MKCircleRenderer(circle: circleOverlay)
+            circleRenderer.fillColor = UIColor(red: 0, green: 0, blue: 1, alpha: 0.3) // 동그라미 내부 색상
+            circleRenderer.strokeColor = UIColor.blue // 동그라미 테두리 색상
+            circleRenderer.lineWidth = 1 // 동그라미 테두리 두께
+            return circleRenderer
         }
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        return MKOverlayRenderer(overlay: overlay)
     }
 }
