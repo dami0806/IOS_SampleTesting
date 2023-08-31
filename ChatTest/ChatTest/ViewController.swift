259634fc914e0b6331d1b4003440fe6afade783c
dami <dami8789@gmail.com>
2023-08-31 23:57:55 +0900

    add Project

---
+++ ChatTest/ChatTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ChatTest
+//
+//  Created by 박다미 on 2023/08/31.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        print("🍎")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
