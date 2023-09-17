d09e7e7892fd11480098f4ff6dc01bf458dcd282
dami <dami8789@gmail.com>
2023-09-17 23:56:10 +0900

    Testing

---
+++ KakaoMap_v2/KakaoMap_v2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  KakaoMap_v2
+//
+//  Created by 박다미 on 2023/09/10.
+//
+
+import UIKit
+import KakaoMapsSDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        
+    }
+
+
+}
+
